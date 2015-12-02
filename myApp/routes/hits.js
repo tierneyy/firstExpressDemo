3fb6fd913cff9eb91d46a8873ca2b822d00494a3
杨鹏涛 <18918578320@189.cn>
2015-12-02 13:01:09 +0800

    the start

---
+++ myApp/routes/hits.js
@@ -0,0 +1,1 @@
+/** * Created by kaka on 2015.12.2. */var hits = 0;exports.count =function(req,res){    res.send(200,{        hits:hits    });}exports.registerNew = function(req,res){    hits += 1;    res.send(200,{hits:hits});}
